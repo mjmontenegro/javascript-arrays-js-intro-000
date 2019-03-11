f1c545a179dd7a4e495ffcaaf2cf73dd2c5d63dd
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-11 22:45:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,25 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element) {
+    return [element, ...array));
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+}
+function accessElementInArray(array, index) {
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+function destructivelyAddElementToEndOfArray(array) {
+  return array.slice(0,-1);
+}
