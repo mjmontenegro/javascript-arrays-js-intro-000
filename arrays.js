bf3104bff460a52d080cbc5b36cabda576a5f031
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-11 22:49:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var addElementToBeginningOfArray = (array, element) => {
-    return [element, ...array));
+    return [element, ...array];
 }
 /*
 function destructivelyAddElementToBeginningOfArray(array, element) {
